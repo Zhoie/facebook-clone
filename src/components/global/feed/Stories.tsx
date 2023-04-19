edc1b8b479c655703c74f03cffb971be4f953911
Niklas <zhoie@icloud.com>
2023-04-19 18:23:30 +0800

    Add: delete post

--- src/components/global/feed/Stories.tsx
+++ src/components/global/feed/Stories.tsx
@@ -11,7 +11,7 @@ const styles = {
     base: 'grid grid-flow grid-cols-1 overflow-x-auto m-2 gap-2 rounded-2xl p-6 bg-zinc-200 border-zinc-950',
     baseAdapt: 'sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5',
   },
-  storyboardContainer: ' bg-zinc-200 shadows-2xl bg-zinc-50 rounded-xl border-2 border-zinc-950',
+  storyboardContainer: ' bg-zinc-200 shadows-2xl bg-zinc-50 rounded-xl border-2 border-zinc-950 trans duration-500',
   // storyboard: {
   //   image: 'rounded-xl filter brightness-75 hover:brightness-100 transition duration-150 transform  cursor-pointer',
   //   name: 'text-zinc-950',
@@ -24,6 +24,7 @@ interface StoryType {
   image: string
   postText: string
   timestamp: Timestamp
+  id: string
 }
 
 export default function Stories() {
@@ -31,14 +32,6 @@ export default function Stories() {
   const [stories, setStories] = useState<StoryType[]>([])
 
 
-  // useEffect(() => {
-  //   const unsubscribe = onSnapshot(collection(db, 'posts'), (snapshot) => {
-  //   setStories(snapshot.docs.map((doc) => doc.data() as StoryType))
-
-  //   })
-  //   return unsubscribe
-
-  // }, [])
 
   useEffect(() => {
     const unsubscribe = onSnapshot(collection(db, 'posts'), (snapshot) => {
@@ -47,6 +40,8 @@ export default function Stories() {
         .sort((a, b) => b.timestamp?.toMillis() - a.timestamp?.toMillis())
 
       setStories(sortedStories)
+
+      console.log(sortedStories)
     })
 
     return unsubscribe
@@ -58,8 +53,9 @@ export default function Stories() {
 
       {stories.map((story, index) => (
         <div className={styles.storyboardContainer} key={index}>
-
-          <Story name={story.name} image={story.image} post={story.postText} />
+            <p>{story.id}</p>
+            
+          <Story name={story.name} timestamp={story.timestamp} email={story.email} image={story.image} post={story.postText} />
           {/* <Image className={styles.storyboard.image} src={story.profile} width={400} height={400} alt={story.name} /> */}
           {/* <h1 className={styles.storyboard.name}>{story.name}</h1> */}
         </div>
--- src/components/global/feed/Story.tsx
+++ src/components/global/feed/Story.tsx
@@ -1,28 +1,65 @@
 import React from 'react'
 import Image from 'next/image'
+import { AiOutlineDelete } from 'react-icons/ai'
+import { useSession } from 'next-auth/react'
+
+
+import { doc, deleteDoc, where, getDocs, query } from 'firebase/firestore'
+
+import { collection, Timestamp } from 'firebase/firestore'
+
+import { db } from '@/db/firebase'
 
 const styles = {
-    container: 'p-1 break-words',
+    container: ' p-1 break-words ',
     post: 'text-2xl',
-    posterContainer: 'mt-6 flex flex-row gap-2',
+    posterContainer: 'mt-6 flex flex-row items-center justify-center gap-8',
     name: 'text-4xl',
-    image: 'rounded-full border-4 border-zinc-950'
+    image: '',
+    imageSize: 20,
+    deleteBtn: 'flex p-2 items-center justify-center w-10 h-10 text-zinc-950 bg-zinc-100 rounded-full border-2 border-zinc-950 shadow-md hover:shadow-lg active:translate-y-1  transition-all duration-200',
 }
 
 interface StoryProps {
     name: string
+    email: string
     post: string
     image: string
+    timestamp: Timestamp
 }
 
-export default function Story({ name, post, image }: StoryProps) {
+export default function Story({ name, post, image, email,timestamp }: StoryProps) {
+
+    const { data: session } = useSession()
+    //delete post
+
+    const deletePost = async () => {
+        if (session?.user?.email === email) {
+            console.log(timestamp)
+            // console.log(name)
+            if (timestamp) { // add null check for timestamp
+                const q = query(collection(db, 'posts'), where('timestamp', '==', timestamp))
+                const querySnapshot = await getDocs(q)
+                querySnapshot.forEach((doc) => {
+                    deleteDoc(doc.ref)
+                })
+            }
+        } else {
+            alert('you are not allowed to delete this post')
+        }
+    }
+
     return (
         <div className={styles.container}>
             <p className={styles.post}>{post}</p>
             <div className={styles.posterContainer}>
-                <Image src={image} width={20} height={20} alt={name} />
+                <div className={styles.image}>
+                    <Image src={image} width={styles.imageSize} height={styles.imageSize} alt={name} />
+                </div>
                 <h3>{name}</h3>
-
+                <button onClick={deletePost} className={styles.deleteBtn} type='button' title='delete'>
+                    <AiOutlineDelete size={styles.imageSize} />
+                </button>
             </div>
 
         </div>
