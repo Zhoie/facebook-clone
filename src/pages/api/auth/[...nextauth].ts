492781cca21ed128e1166a7be2efa6c31004ba7c
Niklas <zhoie@icloud.com>
2023-04-19 17:19:42 +0800

    add: post

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { redirect } from 'next/dist/server/api-utils'
 
 export default NextAuth({
     // Configure one or more authentication providers
